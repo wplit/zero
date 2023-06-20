86e08f9d159f79c298426cf4b2335bf00b321055
David Browne <david@wplit.com>
2023-06-20 12:59:35 +1000

    Update functions.php
    added woocommerce theme support

--- functions.php
+++ functions.php
@@ -26,6 +26,7 @@ function zero_theme_supports() {
     add_theme_support( 'post-thumbnails' );
     add_theme_support( 'title-tag' );
     add_theme_support( 'automatic-feed-links' );
+    add_theme_support( 'woocommerce' );	
     add_theme_support(
         'html5',
         array(
